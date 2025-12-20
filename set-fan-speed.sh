107fe92bb56e4d6082a8c901699ae0c5da117c35
M. <103837701+Miran20122@users.noreply.github.com>
2025-12-20 11:20:45 +0100

    Update set-fan-speed.sh

--- set-fan-speed.sh
+++ set-fan-speed.sh
@@ -1,2 +1,2 @@
-echo 153 > /sys/class/hwmon/hwmon0/device/pwm2  # CPU fan @ 60%
+echo 178 > /sys/class/hwmon/hwmon0/device/pwm2  # CPU fan @ 70%
 echo 200 > /sys/class/hwmon/hwmon0/device/pwm1  # HDD fan @ ~80%
